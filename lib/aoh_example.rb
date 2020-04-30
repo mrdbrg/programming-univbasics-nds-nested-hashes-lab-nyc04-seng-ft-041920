d12cd9776ee0a2724544c1e558fa689fbb638dfc
Marlon Braga <rdg.brg@gmail.com>
2020-04-30 19:10:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,8 +12,9 @@ def assembled_aoh
   a_of_hashes = [
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    
-    
+     { name:  "Pat Monteseian", occupation: "Staff" },
+     { name:  "Kate Gompert", occupation: "None" },
+     { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
